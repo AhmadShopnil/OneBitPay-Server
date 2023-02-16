c484251ecd7edbd5bad9dad8ac8daa41587fbba3
sumaya-kabir <sumayakabir4547@gmail.com>
2023-02-16 12:07:48 +0600

    loan request cancel api

--- index.js
+++ index.js
@@ -603,6 +603,15 @@ async function run() {
             res.send(result)
         });
 
+        // delete/cancel loan request
+        app.delete('/loanRequestList/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await loanApplicantsCollection.deleteOne(filter);
+            res.send(result);
+        });
+       
+
 
 
     }
